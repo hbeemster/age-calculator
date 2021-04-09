66e3d030647152869ccf2078897e0d5460c8332d
herbert <h.beemster@nki.nl>
2021-04-09 11:31:36 +0200

    - setup project

--- age_calculator/__init__.py
+++ age_calculator/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Age Calculator."""
 
 __author__ = """herbert beemster"""
-__email__ = 'h.beemster@nki.nl'
-__version__ = '0.1.0'
+__email__ = "h.beemster@nki.nl"
+__version__ = "0.1.0"
--- age_calculator/age_calculator.py
+++ age_calculator/age_calculator.py
@@ -1,1 +1,13 @@
 """Main module."""
+import arrow
+
+
+def age_in_days(date_of_birth, date_of_death=None):
+    """Calculates the age in days."""
+    date_of_death = arrow.get(date_of_death) if date_of_death else arrow.utcnow()
+    return (date_of_death - arrow.get(date_of_birth)).days
+
+
+def age_in_hours(date_of_birth, date_of_death=None):
+    """Calculates the age in hours."""
+    raise NotImplemented
