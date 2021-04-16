1913662327ef630514b118ab55c9716969f19d0f
marjaneh-T <marjaneh.taghavi@gmail.com>
2021-04-16 16:39:46 +0200

    Add hours

--- age_calculator/age_calculator.py
+++ age_calculator/age_calculator.py
@@ -1,7 +1,6 @@
 """Main module."""
 import arrow
 
-
 def age_in_days(date_of_birth, date_of_death=None):
     """Calculates the age in days."""
     date_of_death = arrow.get(date_of_death) if date_of_death else arrow.utcnow()
@@ -10,4 +9,6 @@ def age_in_days(date_of_birth, date_of_death=None):
 
 def age_in_hours(date_of_birth, date_of_death=None):
     """Calculates the age in hours."""
-    raise NotImplemented
+    date_of_death = arrow.get(date_of_death,'US/Pacific') if date_of_death else arrow.utcnow()
+    return (date_of_death - arrow.get(date_of_birth)).seconds*3600
+
