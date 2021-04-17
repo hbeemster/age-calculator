c0afc094902eb184f2e11d17bb3e307cc9dac088
marjaneh-T <marjaneh.taghavi@gmail.com>
2021-04-17 12:27:40 +0200

    Add hours with the use of age_in_days function

--- age_calculator/age_calculator.py
+++ age_calculator/age_calculator.py
@@ -9,6 +9,5 @@ def age_in_days(date_of_birth, date_of_death=None):
 
 def age_in_hours(date_of_birth, date_of_death=None):
     """Calculates the age in hours."""
-    date_of_death = arrow.get(date_of_death,'US/Pacific') if date_of_death else arrow.utcnow()
-    return (date_of_death - arrow.get(date_of_birth)).seconds*3600
+    return age_in_days(date_of_birth, date_of_death) * 24
 
