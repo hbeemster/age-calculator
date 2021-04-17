c867edb58373b7bd1e21084ed736bf8d67487c31
herbert <h.beemster@nki.nl>
2021-04-17 15:11:29 +0200

    - refactored

--- age_calculator/age_calculator.py
+++ age_calculator/age_calculator.py
@@ -1,13 +1,27 @@
 """Main module."""
 import arrow
 
+
+# ------------------------------------------------------------------------
 def age_in_days(date_of_birth, date_of_death=None):
     """Calculates the age in days."""
-    date_of_death = arrow.get(date_of_death) if date_of_death else arrow.utcnow()
-    return (date_of_death - arrow.get(date_of_birth)).days
+    return date_diff(date_of_birth, date_of_death)
 
 
+# ------------------------------------------------------------------------
 def age_in_hours(date_of_birth, date_of_death=None):
     """Calculates the age in hours."""
-    return age_in_days(date_of_birth, date_of_death) * 24
+    return 24 * date_diff(date_of_birth, date_of_death)
 
+
+# ------------------------------------------------------------------------
+def date_diff(date_from, date_to=None):
+    """Generic function to calculate the difference between two dates
+
+    :param date_from: The start date
+    :param date_to: The end date, default 'Today'
+    :return: The difference in days, int
+
+    """
+    date_to = arrow.get(date_to) if date_to else arrow.utcnow()
+    return (date_to - arrow.get(date_from)).days
